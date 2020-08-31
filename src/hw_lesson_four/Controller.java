f2850f08672266d156e27d22730325e2bd2e5b85
OlgaMArsal <inconcretehome@gmail.com>
2020-08-31 15:13:36 +0300

    Initial commit

---
+++ src/hw_lesson_four/Controller.java
@@ -0,0 +1,15 @@
+package hw_lesson_four;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class Controller {
+
+}
